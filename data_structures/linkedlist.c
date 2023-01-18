29a48903c3211222de4e7f7c52f5cadf83080a11
alex casadio <30955464+nodjiin@users.noreply.github.com>
2023-01-18 19:05:22 +0100

    added linkedlist.c

---
+++ data_structures/linkedlist.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "datatype.h"
+#include "linkedlist.h"
+#include "memalloc.h"
+
+singly_linked_list_t* sllist_construct() {
+	return NULL;
+}
+
+doubly_linked_list_t* dllist_construct() {
+	return NULL;
+}
