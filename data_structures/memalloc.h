119bf20de07d4a531f7ee43b8c4bb195f638b9cf
alex casadio <30955464+nodjiin@users.noreply.github.com>
2023-01-18 18:16:29 +0100

    added memalloc header

---
+++ data_structures/memalloc.h
@@ -0,0 +1,16 @@
+#ifndef MEMALLOC
+#define MEMALLOC
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+	void* malloc_s(size_t size);
+	void* calloc_s(size_t count, size_t size);
+	void* realloc_s(void* block, size_t size);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif // !MEMALLOC
