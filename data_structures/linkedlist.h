de9054e236658681336479d84c6a597b143120ff
alex casadio <30955464+nodjiin@users.noreply.github.com>
2023-01-18 19:06:52 +0100

    added c++ check on linkedlist.h

--- data_structures/linkedlist.h
+++ data_structures/linkedlist.h
@@ -3,27 +3,35 @@
 
 #include "datatype.h"
 
-typedef struct singly_linked_list_t {
-	data_type value;
-	struct singly_linked_list_t* next;
-} singly_linked_list_t;
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
 
-typedef struct doubly_linked_list_t {
-	data_type value;
-	struct doubly_linked_list_t* previous;
-	struct doubly_linked_list_t* next;
-} doubly_linked_list_t;
+	typedef struct singly_linked_list_t {
+		data_type value;
+		struct singly_linked_list_t* next;
+	} singly_linked_list_t;
 
-singly_linked_list_t* sllist_construct();
-void sllist_insert(singly_linked_list_t** list, data_type value);
-void sllist_delete(singly_linked_list_t** list, singly_linked_list_t** node_to_delete);
-singly_linked_list_t* sllist_search(singly_linked_list_t* list, data_type value);
-void sllist_clear(singly_linked_list_t* list);
+	typedef struct doubly_linked_list_t {
+		data_type value;
+		struct doubly_linked_list_t* previous;
+		struct doubly_linked_list_t* next;
+	} doubly_linked_list_t;
 
-doubly_linked_list_t* dllist_construct();
-void dllist_insert(doubly_linked_list_t** list, data_type value);
-void dllist_delete(doubly_linked_list_t** list, doubly_linked_list_t** node_to_delete);
-doubly_linked_list_t* dllist_search(doubly_linked_list_t* list, data_type value);
-void dllist_clear(doubly_linked_list_t* list);
+	singly_linked_list_t* sllist_construct();
+	void sllist_insert(singly_linked_list_t** list, data_type value);
+	void sllist_delete(singly_linked_list_t** list, singly_linked_list_t** node_to_delete);
+	singly_linked_list_t* sllist_search(singly_linked_list_t* list, data_type value);
+	void sllist_clear(singly_linked_list_t* list);
+
+	doubly_linked_list_t* dllist_construct();
+	void dllist_insert(doubly_linked_list_t** list, data_type value);
+	void dllist_delete(doubly_linked_list_t** list, doubly_linked_list_t** node_to_delete);
+	doubly_linked_list_t* dllist_search(doubly_linked_list_t* list, data_type value);
+	void dllist_clear(doubly_linked_list_t* list);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
 
 #endif // !LINKEDLIST
