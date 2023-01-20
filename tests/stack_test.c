bcd0a71626ff781138b570f7c5db4b2b03f0b246
alex casadio <30955464+nodjiin@users.noreply.github.com>
2023-01-20 16:54:25 +0100

    completed stack_test source.

--- tests/stack_test.c
+++ tests/stack_test.c
@@ -5,32 +5,83 @@
 
 void
 stack_construct_test(void) {
-    assert(0);
+    stack_t* stack;
+
+    stack = stack_construct(1);
+
+    assert(stack != NULL);
 }
 
 void
 stack_push_test(void) {
-    assert(0);
+    stack_t* stack;
+
+    stack = stack_construct(2);
+    stack_push(stack, TESTVAL1);
+    stack_push(stack, TESTVAL2);
+
+    assert(stack->array->count == 2);
+    assert(darray_get(stack->array, 0) == TESTVAL1);
+    assert(darray_get(stack->array, 1) == TESTVAL2);
 }
 
 void
 stack_pop_test(void) {
-    assert(0);
+    stack_t* stack;
+    data_type pop1, pop2;
+
+    stack = stack_construct(2);
+    stack_push(stack, TESTVAL1);
+    stack_push(stack, TESTVAL2);
+
+    pop1 = stack_pop(stack);
+    pop2 = stack_pop(stack);
+
+    assert(stack->array->count == 0);
+    assert(pop1 == TESTVAL2);
+    assert(pop2 == TESTVAL1);
 }
 
 void
 stack_count_test(void) {
-    assert(0);
+    stack_t* stack;
+    size_t count;
+
+    stack = stack_construct(2);
+    stack_push(stack, TESTVAL1);
+    stack_push(stack, TESTVAL2);
+
+    count = stack_count(stack);
+
+    assert(count == 2);
 }
 
 void
 stack_is_empty_test(void) {
-    assert(0);
+    stack_t* stack;
+    bool is_empty_pre_push, is_empty_after_push;
+
+    stack = stack_construct(1);
+
+    is_empty_pre_push = stack_is_empty(stack);
+    stack_push(stack, TESTVAL1);
+    is_empty_after_push = stack_is_empty(stack);
+
+    assert(is_empty_pre_push);
+    assert(!is_empty_after_push);
 }
 
 void
 stack_clear_test(void) {
-    assert(0);
+    stack_t* stack;
+
+    stack = stack_construct(2);
+    stack_push(stack, TESTVAL1);
+    stack_push(stack, TESTVAL2);
+
+    stack_clear(&stack);
+
+    assert(stack == NULL);
 }
 
 void
