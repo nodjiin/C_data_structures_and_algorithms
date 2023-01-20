bfcbabfa4e5a3f63176a8fd41f025ac1280330b6
alex casadio <30955464+nodjiin@users.noreply.github.com>
2023-01-20 01:24:00 +0100

    added documentation to datatype

--- data_structures/datatype.h
+++ data_structures/datatype.h
@@ -10,7 +10,17 @@
 extern "C" {
 #endif /* __cplusplus */
 
+/**
+ * \brief           data type of the values contained in all the data structures of this library.
+ */
 typedef int data_type;
+
+/**
+ * \brief           function used to compare 2 data_type values.
+ * \param[in]       a: first value used in the comparison.
+ * \param[in]       2: second value used in the comparison.
+ * \return          a value greater than 0 if a is greater than b, lesser than 0 is a is lesser than b and 0 if a is equal to b.
+ */
 typedef int (*data_type_compare_fn)(const data_type a, const data_type b);
 
 #ifdef __cplusplus
