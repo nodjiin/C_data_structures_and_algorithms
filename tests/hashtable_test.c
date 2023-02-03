faac2954cd8150f6eed8c6fdf2e976037c2a6252
alex casadio <30955464+nodjiin@users.noreply.github.com>
2023-02-03 20:41:25 +0100

    added tests for hashtable

--- tests/hashtable_test.c
+++ tests/hashtable_test.c
@@ -1,11 +1,109 @@
 #include <assert.h>
 #include "data_structures/hashtable.h"
+#include "testvalues.h"
 
-void htable_construct_test(void){ assert(0);}
-void htable_insert_test(void){ assert(0);}
-void htable_delete_test(void){ assert(0);}
-void htable_search_test(void){ assert(0);}
-void htable_clear_test(void){ assert(0);}
+void
+htable_construct_test(void) {
+    hashtable_t* table;
+
+    table = htable_construct(5, 75, hash, key_compare);
+
+    assert(table != NULL);
+    assert(table->buckets != NULL);
+    assert(table->count == 0);
+    assert(table->capacity == 5);
+    assert(table->load_factor == 75);
+    assert(table->load_threshold == 3);
+    assert(table->hash == hash);
+    assert(table->key_compare == key_compare);
+}
+
+void
+htable_insert_test(void) {
+    hashtable_t* table;
+
+    table = htable_construct(5, 75, hash, key_compare);
+
+    htable_insert(table, TESTKEY1, TESTVAL1);
+    htable_insert(table, TESTKEY2, TESTVAL2);
+    htable_insert(table, TESTKEY3, TESTVAL3); /* collision */
+    htable_insert(table, TESTKEY4, TESTVAL4); /* resize */
+    htable_insert(table, TESTKEY5, TESTVAL5);
+    htable_insert(table, TESTKEY5, TESTVAL6); /* overwrite */
+
+    assert(table->count == 5);
+    assert(table->capacity == 11);
+    assert(table->load_threshold == 8);
+
+    /* N.B. the followings asserts are based on prediction done using djb2 as hash function */
+    assert(table->buckets[1]->pair->value == TESTVAL1);
+    assert(table->buckets[3]->pair->value == TESTVAL3);
+    assert(table->buckets[4]->pair->value == TESTVAL4);
+    assert(table->buckets[5]->pair->value == TESTVAL2);
+    assert(table->buckets[5]->next->pair->value == TESTVAL6);
+}
+
+void
+htable_delete_test(void) {
+    hashtable_t* table;
+
+    table = htable_construct(5, 75, hash, key_compare);
+
+    htable_insert(table, TESTKEY1, TESTVAL1);
+    htable_insert(table, TESTKEY2, TESTVAL2);
+    htable_insert(table, TESTKEY3, TESTVAL3);
+    htable_insert(table, TESTKEY4, TESTVAL4);
+    htable_insert(table, TESTKEY5, TESTVAL5);
+
+    htable_delete(table, TESTKEY1);
+    htable_delete(table, TESTKEY5);
+    htable_delete(table, TESTKEY6); /* key not present */
+
+    assert(table->count == 3);
+    assert(table->capacity == 11);
+    assert(table->load_threshold == 8);
+    assert(table->buckets[1] == NULL);
+    assert(table->buckets[5]->next == NULL);
+}
+
+void
+htable_search_test(void) {
+    hashtable_t* table;
+    data_type stored_val1, stored_val2, missing_val;
+
+    table = htable_construct(5, 75, hash, key_compare);
+
+    htable_insert(table, TESTKEY1, TESTVAL1);
+    htable_insert(table, TESTKEY2, TESTVAL2);
+    htable_insert(table, TESTKEY3, TESTVAL3);
+    htable_insert(table, TESTKEY4, TESTVAL4);
+    htable_insert(table, TESTKEY5, TESTVAL5);
+    htable_delete(table, TESTKEY3);
+
+    stored_val1 = htable_search(table, TESTKEY1);
+    stored_val2 = htable_search(table, TESTKEY2);
+    missing_val = htable_search(table, TESTKEY3);
+
+    assert(stored_val1 == TESTVAL1);
+    assert(stored_val2 == TESTVAL2);
+    assert(missing_val == NULL_DATA);
+}
+
+void
+htable_clear_test(void) {
+    hashtable_t* table;
+
+    table = htable_construct(5, 75, hash, key_compare);
+    htable_insert(table, TESTKEY1, TESTVAL1);
+    htable_insert(table, TESTKEY2, TESTVAL2);
+    htable_insert(table, TESTKEY3, TESTVAL3);
+    htable_insert(table, TESTKEY4, TESTVAL4); /* resize */
+    htable_insert(table, TESTKEY5, TESTVAL5);
+
+    htable_clear(&table);
+
+    assert(table == NULL);
+}
 
 void
 hashtable_testall(void) {
--- tests/testvalues.h
+++ tests/testvalues.h
@@ -1,7 +1,9 @@
 #ifndef TESTVALUES
 #define TESTVALUES
 
+#include <string.h>
 #include "data_structures/datatype.h"
+#include "algorithms/hashing.h"
 
 #define TESTVAL1 1
 #define TESTVAL2 2
@@ -13,9 +15,29 @@
 #define TESTVAL8 8
 #define TESTVAL9 9
 
+#define TESTKEY1 "key1"
+#define TESTKEY2 "ky1"
+#define TESTKEY3 "key3"
+#define TESTKEY4 "key4"
+#define TESTKEY5 "key5"
+#define TESTKEY6 "key6"
+#define TESTKEY7 "key7"
+#define TESTKEY8 "key8"
+#define TESTKEY9 "key9"
+
 inline int
 compare(const data_type a, const data_type b) {
     return a - b;
+}
+
+inline uint64_t
+hash(key_type value) {
+    return hash_djb2(value, strlen(value));
+}
+
+inline int
+key_compare(const key_type a, const key_type b) {
+    return strcmp(a, b);
 }
 
 #endif // !TESTVALUES
