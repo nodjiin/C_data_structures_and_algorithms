0bd4a784258f08fe3e7f13ea74dbf75e5b3e4944
alex casadio <30955464+nodjiin@users.noreply.github.com>
2023-02-09 21:44:17 +0100

    added trie test

---
+++ tests/trie_test.c
@@ -0,0 +1,38 @@
+#include <assert.h>
+#include "data_structures/trie.h"
+#include "trie_test.h"
+#include "testvalues.h"
+
+void
+trie_construct_test(void) {
+    assert(0);
+}
+
+void
+trie_insert_test(void) {
+    assert(0);
+}
+
+void
+trie_delete_test(void) {
+    assert(0);
+}
+
+void
+trie_search_test(void) {
+    assert(0);
+}
+
+void
+trie_clear_test(void) {
+    assert(0);
+}
+
+void
+trie_testall(void) {
+    trie_construct_test();
+    trie_insert_test();
+    trie_delete_test();
+    trie_search_test();
+    trie_clear_test();
+}
---
+++ tests/trie_test.h
@@ -0,0 +1,20 @@
+#ifndef TRIE_TEST
+#define TRIE_TEST
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+void trie_construct_test(void);
+void trie_insert_test(void);
+void trie_delete_test(void);
+void trie_search_test(void);
+void trie_clear_test(void);
+
+void trie_testall(void);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif // !TRIE_TEST
