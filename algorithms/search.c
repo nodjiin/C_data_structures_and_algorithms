4fd6fde389a9bd0ba81869c29e81ed6bd5ec772e
alex casadio <30955464+nodjiin@users.noreply.github.com>
2023-02-16 13:04:43 +0100

    added search.h and search.c

---
+++ algorithms/search.c
@@ -0,0 +1,179 @@
+#include <stdio.h>
+#include "search.h"
+#include "data_structures/memalloc.h"
+#include "data_structures/exitcodes.h"
+
+/**
+ * \brief           Constructs a new graph traverser.
+ * \param[in]       graph: pointer to the graph object to be traversed. `NULL` is not considered a valid input and will cause an early exit
+ *                  with INVALID_INPUT status code.
+ * \param[in]       process_vertex_early: pointer to a function that processes a vertex before its adjacent edges.
+ * \param[in]       process_vertex_late: pointer to a function that processes a vertex after its adjacent edges.
+ * \param[in]       process_edge: pointer to a function that processes an.
+ * \return          a pointer to the newly created graph traverser object.
+ * \relates         graph_t
+ */
+graph_traverser_t*
+graph_traverser_construct(graph_t* graph, process_vertex_fn process_vertex_early, process_vertex_fn process_vertex_late,
+                          process_edge_fn process_edge) {
+    graph_traverser_t* new_traverser;
+
+    if (graph == NULL) {
+        fprintf(stderr,
+                "[graph_traverser_construct] Invalid input. Faulty traverse creation request with NULL graph.\n");
+        exit(INVALID_INPUT);
+    }
+
+    new_traverser = malloc_s(sizeof(graph_traverser_t));
+    new_traverser->discovered = calloc_s(graph->nvertices, sizeof(bool));
+    new_traverser->processed = calloc_s(graph->nvertices, sizeof(bool));
+    new_traverser->parent = calloc_s(graph->nvertices, sizeof(size_t));
+    new_traverser->process_edge = process_edge;
+    new_traverser->process_vertex_early = process_vertex_early;
+    new_traverser->process_vertex_late = process_vertex_late;
+
+    return new_traverser;
+}
+
+/**
+ * \brief           free the given graph.
+ * \param[in]       traverser: pointer to graph pointer.
+ * \note            this function will free the memory used by the traverser. The input pointer itself will be set to `NULL`.
+ * \relates         graph_t
+ */
+void
+graph_traverser_clear(graph_traverser_t** traverser) {
+    if (traverser == NULL || *traverser == NULL) {
+        return;
+    }
+
+    free((*traverser)->discovered);
+    free((*traverser)->parent);
+    free((*traverser)->processed);
+    free_s(*traverser);
+}
+
+/**
+ * \brief           constructs a new vertices queue.
+ * \param[in]       size: The maximum number of vertices that can be stored in the queue.
+ * \return          pointer to the newly created vertices queue object.
+ */
+static vertices_queue_t*
+vqueue_construct(size_t size) {
+    vertices_queue_t* new_queue;
+
+    new_queue = malloc_s(sizeof(vertices_queue_t));
+    new_queue->vertices = malloc_s(sizeof(size_t) * size);
+    new_queue->end = 0;
+    new_queue->start = 0;
+
+    return new_queue;
+}
+
+/**
+ * \brief           frees the memory allocated for a vertices queue.
+ * \param[in]       queue: a pointer to the vertices queue to be freed.
+ */
+static void
+vqueue_free(vertices_queue_t* queue) {
+    free(queue->vertices);
+    free(queue);
+}
+
+/**
+ * \brief           adds a vertex to the end of a vertices queue.
+ * \param[in]       queue: A pointer to the vertices queue where the vertex will be added.
+ * \param[in]       vertex: The vertex to be added to the queue.
+ * \hideinitializer
+ */
+#define enqueue(queue, vertex) (queue->vertices[queue->end++] = vertex)
+
+/**
+ * \brief           returns the first vertex of the queue and move its head to the next item in line.
+ * \param           queue: pointer to the vertices queue where the vertex will be removed from.
+ * \return          the vertex that was the first item in the queue.
+ * \hideinitializer
+ */
+#define dequeue(queue)         (queue->vertices[queue->start++])
+
+/**
+ * \brief           checks if a vertices queue is not empty.
+ * \param           queue: pointer to the vertices queue to be checked.
+ * \return          true if there are still elements in the queue, false otherwise.
+ * \hideinitializer
+ */
+#define is_not_empty(queue)    (queue->end > queue->start)
+
+/**
+ * \brief           performs a breadth-first search on a graph object, starting from a given vertex.
+ * \param           graph: pointer to the graph to be searched. `NULL` is not considered a valid input and will cause an early exit
+ *                  with INVALID_INPUT status code.
+ * \param           traverser: pointer to the graph traverser that contains the functions and arrays for processing vertices and edges. `NULL` is not considered
+ *                  a valid input and will cause an early exit with INVALID_INPUT status code.
+ * \param           starting_vertex: index of the vertex where the search will begin. The given value must be a valid index in the graph object.
+ * \note            the graph traverser object is updated with the information gathered during the search, such as parent vertices and processed edges. 
+ *                  The functions provided by the traverser are called at appropriate times during the search.
+ */
+void
+graph_breadth_first_search(graph_t* graph, graph_traverser_t* traverser, size_t starting_vertex) {
+    vertices_queue_t* queue;
+    size_t source_vertex, destination_vertex;
+    edgenode_t* edge;
+
+    if (graph == NULL) {
+        fprintf(stderr, "[graph_breadth_first_search] Invalid input. Faulty bfs search on NULL graph.\n");
+        exit(INVALID_INPUT);
+    }
+
+    if (traverser == NULL) {
+        fprintf(stderr, "[graph_breadth_first_search] Invalid input. Faulty bfs search with NULL traverser.\n");
+        exit(INVALID_INPUT);
+    }
+
+    if (starting_vertex >= graph->nvertices) {
+        fprintf(stderr,
+                "[graph_breadth_first_search] Invalid input. Faulty bfs search with starting vertex '%zu' "
+                "exceeding vertices size '%zu'.\n",
+                starting_vertex, graph->nvertices);
+        exit(INVALID_INPUT);
+    }
+
+    queue = vqueue_construct(graph->nvertices);
+    enqueue(queue, starting_vertex);
+    traverser->discovered[starting_vertex] = true;
+
+    while (is_not_empty(queue)) {
+        source_vertex = dequeue(queue);
+
+        if (traverser->process_vertex_early != NULL) {
+            traverser->process_vertex_early(source_vertex);
+        }
+
+        traverser->processed[source_vertex] = true;
+        edge = graph->edges[source_vertex];
+
+        while (edge != NULL) {
+            destination_vertex = edge->y;
+
+            if ((!traverser->processed[destination_vertex]) || graph->is_directed) {
+                if (traverser->process_edge != NULL) {
+                    traverser->process_edge(source_vertex, edge);
+                }
+            }
+
+            if (!traverser->discovered[destination_vertex]) {
+                enqueue(queue, destination_vertex);
+                traverser->discovered[destination_vertex] = true;
+                traverser->parent[destination_vertex] = source_vertex;
+            }
+
+            edge = edge->next;
+        }
+
+        if (traverser->process_vertex_late != NULL) {
+            traverser->process_vertex_late(source_vertex);
+        }
+    }
+
+    vqueue_free(queue);
+}
---
+++ algorithms/search.h
@@ -0,0 +1,68 @@
+/**
+ * \file            search.h
+ * \brief           header file containing the declaration of search algorithms implemented in this library.
+ */
+
+#ifndef SEARCH
+#define SEARCH
+
+#include <stdlib.h>
+#include "data_structures/graph.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+/**
+ * \brief           structure that represents a queue of vertices.
+ * \note            this structure is used to store the vertices that need to be visited during graph traversal.
+ */
+typedef struct {
+    size_t* vertices; /*!< array of vertices. */
+    size_t start;     /*!< index of the first element in the queue */
+    size_t end;       /*!< index of the last element in the queue */
+} vertices_queue_t;
+
+/**
+* \brief            function pointer type that defines a callback for processing a vertex.
+* \note             this type is used to pass a function that performs some action on a vertex during graph traversal.
+*                   The function takes a size_t parameter that represents the vertex number, and returns void
+*/
+typedef void (*process_vertex_fn)(size_t vertex);
+
+/**
+* \brief            function pointer type that defines a callback for processing an edge.
+*                   this type is used to pass a function that performs some action on an edge during graph traversal.
+*                   The function takes two parameters: a size_t parameter that represents the source vertex number, and an edgenode_t pointer
+*                   that represents the edge node. The function returns void.
+*/
+typedef void (*process_edge_fn)(size_t vertex, edgenode_t* edge);
+
+/**
+* \brief            structure that represents a graph traverser object.
+* \note             this structure is used to store the state and callbacks for graph traversal algorithms such as breadth-first search or depth-first search.
+*/
+typedef struct graph_traverser_t {
+    bool* processed;  /*!< array that indicate whether a vertex has been processed. */
+    bool* discovered; /*!< array that indicate whether a vertex has been discovered*/
+    size_t* parent;   /*!< array containing the number of the parent of each vertex */
+    process_vertex_fn
+        process_vertex_early; /*!< function pointer that defines how to process a vertex before its adjacent edges */
+    process_vertex_fn
+        process_vertex_late;      /*!< function pointer that defines how to process a vertex after its adjacent edges */
+    process_edge_fn process_edge; /*!< function pointer that defines how to process an edge */
+} graph_traverser_t;
+
+__declspec(dllexport) graph_traverser_t* graph_traverser_construct(graph_t* graph,
+                                                                   process_vertex_fn process_vertex_early,
+                                                                   process_vertex_fn process_vertex_late,
+                                                                   process_edge_fn process_edge);
+__declspec(dllexport) void graph_traverser_clear(graph_traverser_t** traverser);
+__declspec(dllexport) void graph_breadth_first_search(graph_t* graph, graph_traverser_t* traverser,
+                                                      size_t starting_vertex);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif // !SEARCH
