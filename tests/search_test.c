88187be4c54b882548bf44479d093eca201adcc2
alex casadio <30955464+nodjiin@users.noreply.github.com>
2023-02-28 19:37:53 +0100

    search_test: added partial breadth-first and depth-first search implementations

--- tests/search_test.c
+++ tests/search_test.c
@@ -20,27 +20,26 @@ setup() {
     *  graph structure:
     *  
     *   3  4 <---- 5  6
-    *   ^  ^       ^  ^
-    *    \ |       | /
+    *   ^  ^       ^   
+    *    \ |       |  
     *      1 ----> 2
     *       ^     ^
     *        \   /
     *          0 
     */
     graph = graph_construct(7, true);
-    graph_insert_edge(graph, 0, 1, 0);
     graph_insert_edge(graph, 0, 2, 0);
-    graph_insert_edge(graph, 1, 2, 0);
-    graph_insert_edge(graph, 1, 3, 0);
+    graph_insert_edge(graph, 0, 1, 0);
     graph_insert_edge(graph, 1, 4, 0);
+    graph_insert_edge(graph, 1, 3, 0);
+    graph_insert_edge(graph, 1, 2, 0);
     graph_insert_edge(graph, 2, 5, 0);
-    graph_insert_edge(graph, 2, 6, 0);
     graph_insert_edge(graph, 5, 4, 0);
 }
 
 void
 tear_down() {
-    graph_clear(graph);
+    graph_clear(&graph);
 }
 
 void
@@ -99,12 +98,44 @@ graph_traverser_clear_test(void) {
 
 void
 graph_breadth_first_search_test(void) {
-    assert(0);
+    graph_traverser_t* traverser;
+
+    traverser = graph_traverser_construct(graph, pv_early, pv_late, pe, BREADTH_FIRST);
+
+    graph_breadth_first_search(graph, traverser, 0);
+
+    assert(traverser->discovered[0]);
+    assert(traverser->parent[0] == GRAPH_PARENT_UNDEFINED);
+    assert(traverser->parent[1] == 0);
+    assert(traverser->parent[2] == 0);
+    assert(traverser->parent[3] == 1);
+    assert(traverser->parent[4] == 1);
+    assert(traverser->parent[5] == 2);
+    assert(traverser->parent[6] == GRAPH_PARENT_UNDEFINED);
+    assert(!traverser->discovered[6]);
+
+    graph_traverser_clear(&traverser);
 }
 
 void
 graph_depth_first_search_test(void) {
-    assert(0);
+    graph_traverser_t* traverser;
+
+    traverser = graph_traverser_construct(graph, pv_early, pv_late, pe, DEPTH_FIRST);
+
+    graph_depth_first_search(graph, traverser, 0);
+
+    assert(traverser->discovered[0]);
+    assert(traverser->parent[0] == GRAPH_PARENT_UNDEFINED);
+    assert(traverser->parent[1] == 0);
+    assert(traverser->parent[2] == 1);
+    assert(traverser->parent[5] == 2);
+    assert(traverser->parent[4] == 5);
+    assert(traverser->parent[3] == 1);
+    assert(traverser->parent[6] == GRAPH_PARENT_UNDEFINED);
+    assert(!traverser->discovered[6]);
+
+    graph_traverser_clear(&traverser);
 }
 
 void
