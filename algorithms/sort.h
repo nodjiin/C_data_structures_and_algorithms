18f49ab5de5d50f008257e789a5e8bbbd01038aa
alex casadio <30955464+nodjiin@users.noreply.github.com>
2023-03-11 12:49:30 +0100

    added sort.h to algorithms project

---
+++ algorithms/sort.h
@@ -0,0 +1,22 @@
+/**
+ * \file            sort.h
+ * \brief           header file containing the declaration of sort algorithms implemented in this library.
+ */
+
+#ifndef SORT
+#define SORT
+
+#include <stdlib.h>
+#include <core/datatype.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+__declspec(dllexport) void selection_sort(data_type array[], size_t array_size);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif // !SORT
